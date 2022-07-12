af741e1b56ad63b4cbd2c34be0ae375e6f8727fa
Anna Wilson <git@annawilson.eu>
2022-07-12 13:44:51 +0100

    store images as media

--- swapshop/items/models.py
+++ swapshop/items/models.py
@@ -4,12 +4,19 @@ class Item(models.Model):
     name = models.CharField(max_length=200)
     available = models.BooleanField(default=False)
 
+    def __str__(self):
+        return self.name
+
 class ItemImage(models.Model):
+    img = models.ImageField(null=True)
     item = models.ForeignKey(
         'Item',
         on_delete = models.CASCADE
     )
     desc = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.item.name
 
 class Comment(models.Model):
     item = models.ForeignKey(
@@ -17,3 +24,5 @@ class Comment(models.Model):
         on_delete = models.CASCADE
     )
     text = models.TextField()
+
+
