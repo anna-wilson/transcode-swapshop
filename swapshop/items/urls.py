00a3e5b79bf728d7f4d6fe1dfa8b3f57f02c3681
ElyseRH <elyserhoran@gmail.com>
2022-07-12 15:24:45 +0000

    Add views for image and comments

--- swapshop/items/urls.py
+++ swapshop/items/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('add_item/', views.add_item, name='add_item'),
-    # path('add_item/', views.add_item_image, name='add_item_image'),
+    path('add_item_image/<item_id>', views.add_item_image, name='add_item_image'),
+    path('item_detail/<item_id>', views.item_detail, name='item_detail'),
 ]
